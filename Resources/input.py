08eb8a09c0e2a40b9ba7189e5e6c6865f9633cda
Damian Kolaska <damian.kolaska99@gmail.com>
2021-04-02 12:55:07 +0200

    Project structure

---
+++ Resources/input.py
@@ -0,0 +1,2 @@
+﻿def hello():
+    pass
---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PythonCSharpTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
